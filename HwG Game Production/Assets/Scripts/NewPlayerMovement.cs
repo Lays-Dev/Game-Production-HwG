44ade9a317377f6db2f41534671a53c6a603877a
Lays-Dev <penn2ink@gmail.com>
2026-02-21 19:33:05 -0500

    Item Implementation

---
+++ HwG Game Production/Assets/Scripts/NewPlayerMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class NewPlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpHeight = 1.5f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Check if grounded
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Movement input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
--- HwG Game Production/Assets/Scripts/PickUpItem.cs
+++ HwG Game Production/Assets/Scripts/PickUpItem.cs
@@ -1,23 +1,46 @@
 // If item is static
 // > No Nav-Mesh Obstacle component
 // > If it does have Nav-Mesh then distroy it on pickup
+// Data Chip must have a collider with Is Trigger checked
+// Data Chip should have a rigidbody kinematic checked
+// Player must have Character Controller
+// Player must have Player tag
 using UnityEngine;
 
 public class PickupItem : MonoBehaviour
 {
-    // Assign the enemy here
-    public EnemySound enemyScript; 
+    // Variables
+    // Enemy goes here
+    public EnemySound Enemy;
+
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            // Alert the enemy that item was picked up
+            Enemy.AlertToPlayerLocation();
+            // Pick up the item
+            Debug.Log("Item picked up! Enemy is coming");
+            Destroy(gameObject);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Something entered trigger: " + other.name);
         if (other.CompareTag("Player"))
         {
-            // Alert the enemy
-            enemyScript.AlertToPlayerLocation();
+            playerInRange = true;
+        }
+    }
 
-            // Pick up the item
-            Debug.Log("Item picked up! Enemy is coming...");
-            Destroy(gameObject); 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
         }
     }
 }
