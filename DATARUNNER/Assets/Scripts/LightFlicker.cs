3cec833c6cf2b1171368a3d8ddf6c85d785fbe61
azsreth <nelsononeillgarcia42@gmail.com>
2026-02-28 00:17:27 -0500

    Lighting and shadow update + blinking light script

--- DATARUNNER/Assets/Scripts/LightFlicker.cs
+++ DATARUNNER/Assets/Scripts/LightFlicker.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class LightFlicker : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Light light;
+    
+    public float minIntensity = .5f;
+    public float maxIntensity = 5.0f;
+    public float flickerSpeed = 0.14f;
+
+    private void Start()
     {
-        
+        light = GetComponent<Light>();
+
+        InvokeRepeating("Flicker", 0f, flickerSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Flicker()
     {
-        
+        float randomIntensity = Random.Range(minIntensity, maxIntensity);
+        light.intensity = randomIntensity;
     }
 }
