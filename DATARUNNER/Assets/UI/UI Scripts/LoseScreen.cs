660311ce3a5d5420edc7da72af3f5528700fe4b1
Jeremy Cruz <friedchipsjc@gmail.com>
2026-03-01 22:06:34 -0500

    Main UIs & Functionality Implemented
    Main UI is established with functionality. HUD remains and Win and Lose screens await their conditions to be displayed to the player.

---
+++ DATARUNNER/Assets/UI/UI Scripts/LoseScreen.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScreen : MonoBehaviour
+{
+    public GameObject gameLose_UI;
+
+    void Start()
+    {
+    }
+
+    public void RetryButton()
+    {
+        SceneManager.LoadScene("DATARUNNER");
+        AudioListener.pause = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        AudioListener.pause = false;
+    }
+}
---
+++ DATARUNNER/Assets/UI/UI Scripts/WinScreen.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    public GameObject gameWin_UI;
+
+    void Start()
+    {
+    }
+
+    public void RetryButton()
+    {
+        SceneManager.LoadScene("DATARUNNER");
+        AudioListener.pause = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        AudioListener.pause = false;
+    }
+}
